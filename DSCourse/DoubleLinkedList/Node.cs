61eab5bf75f2c9f45b7dd59b615ab4e5e0642cf3
Dibyendu Tiwary <Dibyendu.Tiwary@Fiserv.com>
2021-08-12 15:18:37 -0700

    QueueAsDouble Linked list.

--- DSCourse/DoubleLinkedList/Node.cs
+++ DSCourse/DoubleLinkedList/Node.cs
@@ -19,4 +19,5 @@ namespace DSCourse.DoubleLinkedList
             this.Value = value;
         }
     }
+
 }
---
+++ DSCourse/QueueAsDoubleLinkedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace DSCourse
+{
+
+    public class QueueNode
+    {
+        public QueueNode Prev { get; set; }
+
+        public QueueNode Next { get; set; }
+
+        public int Value { get; set; }
+
+        public QueueNode(int value)
+        {
+            this.Prev = null;
+            this.Next = null;
+            this.Value = value;
+        }
+
+    }
+
+    public class QueueAsDoubleLinkedList
+    {
+        private QueueNode _head = null;
+        private QueueNode _tail = null;
+
+        public QueueAsDoubleLinkedList()
+        {
+            this._head = null;
+            this._tail = null;
+        }
+
+        public void Enqueue(params int[] values)
+        {
+            foreach (var value in values)
+            {
+                var newNode = new QueueNode(value);
+                // First Node 
+                if (this._head == null)
+                {
+                    // Set the head and tail to the new node
+                    this._head = this._tail = newNode;
+                }
+                else
+                {
+                    // New node makes it to the head. 
+                    newNode.Next = this._head;
+                    this._head.Prev = newNode;
+                    this._head = newNode;
+                }
+            }
+        }
+        
+        public int Dequeue()
+        {
+            if (this._head == null || this._tail == null)
+            {
+                throw new Exception("Empty queue.");
+            }
+            else
+            {
+                var valueToBeDequeued = this._tail.Value;
+                this._tail = this._tail.Prev;
+                this._tail.Next = null;
+                return valueToBeDequeued;
+            }
+        }
+
+
+    }
+}
